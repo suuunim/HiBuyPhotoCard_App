9b9ba7cc6719d9090d8fca03dc14335e5107edaf
suuunim <sua3671@gmail.com>
2021-08-29 13:18:38 +0900

    feature/sign-up
    08/29 : 아이콘 수정
            아이돌 선택
            팬덤명

--- app/src/main/java/com/example/hibuyphotocard/MainActivity.java
+++ app/src/main/java/com/example/hibuyphotocard/MainActivity.java
@@ -39,53 +39,9 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
 
 
-        bottomNavigationView=findViewById(R.id.navigation);
-        menu=bottomNavigationView.getMenu();
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
-        bottomNavigationView.setSelectedItemId(R.id.house);  //선택된 아이템 지정
-
     }
 
 
-    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            FragmentTransaction transaction = manager.beginTransaction();
-            switch(menuItem.getItemId())
-            {
-                case R.id.write:
 
-                    menu.findItem(R.id.house).setIcon(R.drawable.house);
-                    menu.findItem(R.id.my_page).setIcon(R.drawable.logo_icon);
-                    menu.findItem(R.id.chat).setIcon(R.drawable.chat);
-                    transaction.replace(R.id.frameLayout, fragment1).commitAllowingStateLoss();
-                    break;
-
-                case R.id.house:
-
-                    menu.findItem(R.id.write).setIcon(R.drawable.contract);
-                    menu.findItem(R.id.my_page).setIcon(R.drawable.logo_icon);
-                    menu.findItem(R.id.chat).setIcon(R.drawable.chat);
-                    transaction.replace(R.id.frameLayout, fragment2).commitAllowingStateLoss();
-                    break;
-
-                case R.id.my_page:
-                    menu.findItem(R.id.house).setIcon(R.drawable.house);
-                    menu.findItem(R.id.write).setIcon(R.drawable.contract);
-                    menu.findItem(R.id.chat).setIcon(R.drawable.chat);
-                    transaction.replace(R.id.frameLayout, fragment4).commitAllowingStateLoss();
-
-                    break;
-
-                case R.id.chat:
-                    menu.findItem(R.id.house).setIcon(R.drawable.house);
-                    menu.findItem(R.id.write).setIcon(R.drawable.contract);
-                    menu.findItem(R.id.my_page).setIcon(R.drawable.logo_icon);
-                    transaction.replace(R.id.frameLayout, fragment3).commitAllowingStateLoss();
-            }// switch()..
-            return true;
-        }
-    }// ItemSelectedListener class..
 
 }
